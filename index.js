ad725f40deb4849d0be50420c9a980fec5e49005
Philippe <philippe@lucidar.me>
2023-04-12 13:52:49 +0200

    Simple code

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
+
 const http = require('http');
 const server = http.createServer(app);
+
 const { Server } = require("socket.io");
 const io = new Server(server);   // For getting path to the public folder
-
-
 
 
 // Get current path and server static page
@@ -14,28 +14,22 @@ app.use(express.static('public'));
 
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
     
-    socket.on('message', (msg) => {
-        console.log (msg);
-      });
-
-    setInterval(() => {
-        socket.emit('label', new Date);
-    }, 1000)
-
-
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
+    // A new client is connecter
+    console.log('A new client is connected');
+    
+    // If a message is received from the client, display the message in the console
+    socket.on('clientToServer', (content) => {
+        console.log ('Message from client : ',content);
     });
-  });
+
+    // Every second, send a message with the date
+    setInterval(() => {
+        socket.emit('serverToClient', new Date);
+    }, 1000)
+});
 
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
-  });
-/*
-// Start the server
-app.listen(3000, function() {
-  console.log ('Server started on http://localhost:3000');
-});*/
+});
